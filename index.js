cfb087eba05260648460a07d9f6edef646240e76
Yvette Hernandez <yhern006@users.noreply.github.com>
2026-02-10 13:39:40 -0800

    fetch color api successful

---
+++ index.js
@@ -0,0 +1,16 @@
+const seedColorEl = document.getElementById('seed-color')
+const schemeModeEl = document.getElementById('scheme-mode-select')
+const getSchemeBtn = document.getElementById('get-scheme-btn')
+
+getSchemeBtn.addEventListener('click', fetchColorScheme)
+
+function fetchColorScheme() {
+    const seedColorClean = seedColorEl.value.slice(-6)
+    const schemeModeClean = schemeModeEl.value.toLowerCase()
+    console.log(schemeModeClean)
+    fetch(`https://www.thecolorapi.com/scheme?hex=${seedColorClean}&format=json&mode=${schemeModeClean}`)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+
+// ex: https://www.thecolorapi.com/scheme?hex=0047AB&format=html&mode=analogic&count=6
